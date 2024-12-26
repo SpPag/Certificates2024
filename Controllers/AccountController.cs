0e83262630898eb37b83a322757c201665f48030
SpPag <spiridonpagiatis@gmail.com>
2024-12-26 15:40:35 +0200

    Added Register action in AccountController, RegisterVM and basic Register and RegisterCompleted views , Moved Login, Register, Logout buttons from _Layout to _Identity, Added Logout action in AccountController, Changed which buttons appear while logged out / in, Set certain buttons to only appear if authenticated and admin

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Certificates2024.Data;
+using Certificates2024.Data.Static;
 using Certificates2024.Data.ViewModels;
 using Certificates2024.Models;
 using Microsoft.AspNetCore.Identity;
@@ -45,6 +46,43 @@ namespace Certificates2024.Controllers
             }
             TempData["Error"] = "Wrong credentials, email not found. Please, try again!";
             return View(loginVM);
+        }
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home"); //action, controller
         }
     }
 }
