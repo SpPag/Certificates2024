3f9f601214585509af13e51e62498f84b9ee3aa3
SpPag <spiridonpagiatis@gmail.com>
2024-12-26 16:27:47 +0200

    fixed the dropdown menu for creating a new Topic that I broke

--- obj/Debug/net8.0/Certificates2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Certificates2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Certificates2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4eaaaededc3f96b3535ba85ae3a5e0de370c90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e83262630898eb37b83a322757c201665f48030")]
 [assembly: System.Reflection.AssemblyProductAttribute("Certificates2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Certificates2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
