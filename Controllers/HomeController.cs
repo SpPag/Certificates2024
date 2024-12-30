43b41a97c9b3fdf44567734328a3d4c9e6d44d57
CharaSkr <chara.sk84@gmail.com>
2024-12-30 22:14:19 +0200

    Editing Controllers/HomeController.cs Views/Home/index.cshtml Views/CandidateCertificates/Create.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,26 @@
-using System.Diagnostics;
+using Certificates2024.Data.Services;
 using Certificates2024.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Certificates2024.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICertificateTopicsService _certificateTopicsService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICertificateTopicsService certificateTopicsService)
         {
             _logger = logger;
+            _certificateTopicsService = certificateTopicsService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var topics = await _certificateTopicsService.GetAllAsync();
+            return View(topics);
         }
 
         public IActionResult Privacy()
