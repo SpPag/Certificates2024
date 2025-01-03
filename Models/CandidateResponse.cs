3cd7ecef745d589c219dc04788d76027d686ee5d
Superskilos <giannismichalakeas@gmail.com>
2025-01-03 18:43:54 +0200

    After Test show at TestResult View True or False answers!

--- Models/CandidateResponse.cs
+++ Models/CandidateResponse.cs
@@ -18,5 +18,17 @@ namespace Certificates2024.Models
         public Question Question { get; set; }
 
         public string SelectedAnswer { get; set; } // Candidate's selected answer
+        public bool IsCorrect
+        {
+            get
+            {
+                if (Question == null) return false; // Ensure Question is not null
+                if (SelectedAnswer == Question.AnswerA && Question.BooleanA) return true;
+                if (SelectedAnswer == Question.AnswerB && Question.BooleanB) return true;
+                if (SelectedAnswer == Question.AnswerC && Question.BooleanC) return true;
+                if (SelectedAnswer == Question.AnswerD && Question.BooleanD) return true;
+                return false;
+            }
+        }
     }
 }
